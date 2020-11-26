d7189866c498000081ba5083726d0ccefcfb684a
sprinzs123 <sprinzs351@gmail.com>
2020-11-26 16:15:53 -0500

    name property adding correctly to new fields

--- Desktop/PycharmProjects/job/server_app.py
+++ Desktop/PycharmProjects/job/server_app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        print('is it working')
+        return render_template('home.html')
+    else:
+        return render_template('home.html')
+
+
+@app.route('/results')
+def result():
+    return render_template('results.html')
 
 
 if __name__ == '__main__':
--- Desktop/PycharmProjects/job/static/script.js
+++ Desktop/PycharmProjects/job/static/script.js
@@ -3,11 +3,14 @@ function addFields() {
     let allAddBtns = document.querySelectorAll('.filter-btn')
     allAddBtns.forEach((addField) => {
         addField.addEventListener('click', function (event){
+            let filterCount =getFilterNums(addField)
+            let filerType = getColumnFilter(addField)
+            let filterName = filerType + '-' + filterCount
             let parentDiv = addField.parentElement.parentElement
             let newItem = document.createElement('li');
             newItem.classList = 'one-box'
             let newField = `
-                <input type="text" class='filter' placeholder="filter here">
+                <input type="text" class='filter' placeholder="filter here" name='${filterName}'>
                 <button class='delete-btn'>X</button>
             `
             newItem.innerHTML = newField
@@ -25,11 +28,26 @@ addFields()
 function removeFields(){
     let allRemoveBtns = document.querySelectorAll('.delete-btn')
     allRemoveBtns.forEach((removeBtn) => {
-        console.log(removeBtn)
         removeBtn.addEventListener('click', function(event){
-
             let boxItem = removeBtn.parentElement.remove()
-            // parentDiv.removeChild(boxItem)
         })
     })
 }
+
+
+// get how many filters input recorded for each category
+// need in creation of names for new filter inputs
+function getFilterNums(childElement){
+    let parentList = childElement.parentElement.parentElement
+    return parentList.childElementCount
+}
+
+
+// determine to what column filter belongs to 
+function getColumnFilter(childElement){
+    let oneColumn = childElement.parentElement.parentElement
+    return oneColumn.id
+}
+
+
+
