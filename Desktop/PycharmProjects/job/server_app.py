094ad3ea9a988440eaf69f6b562643d56b1029c4
sprinzs123 <sprinzs351@gmail.com>
2020-12-08 17:43:09 -0500

    get all filters dictionaries

--- Desktop/PycharmProjects/job/server_app.py
+++ Desktop/PycharmProjects/job/server_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from connected_scrape import dummy_items
+from connected_scrape import all_data
 
 
 app = Flask(__name__)
@@ -8,7 +8,24 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        print('is it working')
+        input_dictionary = request.form
+        title_filters = []
+        requiremets_filters = []
+        summary_filter = []
+        any_filters = []
+        for field in input_dictionary:
+            if 'title' in field:
+                title_filters.append(input_dictionary.get(field))
+            elif 'requirement' in field:
+                requiremets_filters.append(input_dictionary.get(field))
+            elif 'summary' in field:
+                summary_filter.append(input_dictionary.get(field))
+            elif 'any' in field:
+                any_filters.append(input_dictionary.get(field))
+
+
+
+
         return render_template('home.html')
     else:
         return render_template('home.html')
